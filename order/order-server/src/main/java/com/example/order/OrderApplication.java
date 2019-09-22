b8baf12662c8580f90f8d3f1653dd97643aa7d4c
zhangke <changhe626@163.com>
2019-09-22 16:51:50 +0800

    学习完整个教程了,还有很多的事要做,总结一遍 2019年9月22日16:51:24

--- order/order-server/src/main/java/com/example/order/OrderApplication.java
+++ order/order-server/src/main/java/com/example/order/OrderApplication.java
@@ -2,8 +2,6 @@ package com.example.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.SpringCloudApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
